acd80bd8049db436d37c33a4feeed9559a7fec16
KenV99 <KenV99@users.noreply.github.com>
2014-11-24 06:11:02 -0500

    update mediainfofromlog

--- resources/lib/mediainfofromlog.py
+++ resources/lib/mediainfofromlog.py
@@ -22,7 +22,7 @@ import json
 
 def get_log_mediainfo():
     """
-    Retrieves dimensions and framerate information from XBMC.log
+    Retrieves dimensions and framerate information from XBMC.log or kodi.log
     Will likely fail if XBMC in debug mode - could be remedied by increasing the number of lines read
     Props: http://stackoverflow.com/questions/260273/most-efficient-way-to-search-the-last-x-lines-of-a-file-in-python
     @return: dict() object with the following keys:
@@ -35,40 +35,57 @@ def get_log_mediainfo():
                                 'fps' (float)
     @rtype: dict()
     """
-    logfn = xbmc.translatePath(r'special://home\XBMC.log')
-    xbmc.sleep(250)  # found originally that it wasn't written yet
+    exec_version = float(str(xbmc.getInfoLabel("System.BuildVersion"))[0:4])
+    if exec_version < 14.0:
+        logfn = xbmc.translatePath(r'special://logpath/xbmc.log')
+    else:
+        logfn = xbmc.translatePath(r'special://logpath/kodi.log')
     if is_xbmc_debug():
         lookbacksize = 6144
         lookbacklines = 60
     else:
         lookbacksize = 2560
         lookbacklines = 25
-    with open(logfn, "r") as f:
-        f.seek(0, 2)           # Seek @ EOF
-        fsize = f.tell()        # Get Size
-        f.seek(max(fsize - lookbacksize, 0), 0)  # Set pos @ last n chars
-        lines = f.readlines()       # Read to end
-    lines = lines[-lookbacklines:]    # Get last n lines
     ret = None
-    for line in lines:
-        if 'fps:' in line:
-            start = line.find('fps:')
-            sub = line[start:].rstrip('\n')
-            tret = dict(item.split(":") for item in sub.split(","))
-            ret = {}
-            for key in tret:
-                tmp = key.strip()
-                try:
-                    if tmp == 'fps':
-                        ret['fps'] = float(tret[key])
-                    else:
-                        ret[tmp] = int(tret[key])
-                except ValueError:
-                    pass
-            if ret['pheight'] != 0:
-                ret['par'] = float(ret['pwidth'])/float(ret['pheight'])
-            if ret['dheight'] != 0:
-                ret['dar'] = float(ret['dwidth'])/float(ret['dheight'])
+    numretries = 4
+    while numretries > 0:
+        xbmc.sleep(250)
+        try:
+            with open(logfn, "r") as f:
+                f.seek(0, 2)           # Seek @ EOF
+                fsize = f.tell()        # Get Size
+                f.seek(max(fsize - lookbacksize, 0), 0)  # Set pos @ last n chars
+                lines = f.readlines()       # Read to end
+            lines = lines[-lookbacklines:]    # Get last n lines
+
+            for line in lines:
+                if 'fps:' in line:
+                    start = line.find('fps:')
+                    sub = line[start:].rstrip('\n')
+                    tret = dict(item.split(":") for item in sub.split(","))
+                    ret = {}
+                    for key in tret:
+                        tmp = key.strip()
+                        try:
+                            if tmp == 'fps':
+                                ret['fps'] = float(tret[key])
+                            else:
+                                ret[tmp] = int(tret[key])
+                        except ValueError:
+                            pass
+                    if ret['pheight'] != 0:
+                        ret['par'] = float(ret['pwidth'])/float(ret['pheight'])
+                    if ret['dheight'] != 0:
+                        ret['dar'] = float(ret['dwidth'])/float(ret['dheight'])
+        except Exception as e:
+            xbmc.log('Error opening logfile: {0}'.format(logfn))
+            if hasattr(e, 'message'):
+                xbmc.log('Error message: {0}'.format(e.message))
+            numretries = 0
+        if ret is not None:
+            numretries = 0
+    if ret is None:
+        xbmc.log('Could not retrieve video info from log')
     return ret
 
 
